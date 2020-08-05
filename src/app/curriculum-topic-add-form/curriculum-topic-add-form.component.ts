737d6fb2dfb6545a7b59a00db3787ba3adbd4f27
Kezik.92 <Kezik.92@LAPTOP-4>
2020-08-05 17:25:53 +0300

    v 0.8

--- src/app/curriculum-topic-add-form/curriculum-topic-add-form.component.ts
+++ src/app/curriculum-topic-add-form/curriculum-topic-add-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-curriculum-topic-add-form',
@@ -6,15 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./curriculum-topic-add-form.component.scss']
 })
 export class CurriculumTopicAddFormComponent implements OnInit {
+form: FormGroup;
+  public addresses: any[] = [{
+    address: '',
+    street: '',
+    city: '',
+    country: ''
+  }];
 
-  constructor() { }
 
   ngOnInit(): void {
-
+this.form = new FormGroup( {
+  skills: new FormArray([])
+});
   }
 
   // tslint:disable-next-line:typedef
   getEdit() {
     console.log('fdfdfd');
+    const control = new FormControl();
+    (this.form.get('skills') as FormArray).push(control);
+
+    this.addresses.push({
+      address: '',
+      street: '',
+      city: '',
+      country: ''
+    });
   }
 }
