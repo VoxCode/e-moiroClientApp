6369eae61390945d003f9df2473640ae48195a3c
Kezik.92 <Kezik.92@LAPTOP-4>
2020-08-31 17:17:16 +0300

    v 0.9

--- src/app/group/group-add.component.ts
+++ src/app/group/group-add.component.ts
@@ -2,16 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import { GroupService } from '../services/group.service';
 import { Group } from '../models/Group';
+import {FormOfEducation} from '../models/FormOfEducation';
+import {FormOfEducationService} from '../services/form-of-education.service';
 
 @Component({
   selector: 'app-group',
   templateUrl: './group-add.component.html',
   styleUrls: ['./group-add.component.scss'],
-  providers: [GroupService]
+  providers: [
+    GroupService,
+    FormOfEducationService ]
 })
 export class GroupAddComponent implements OnInit {
   value: Group = new Group();
   values: Group[];
+
+  formOfEducation: FormOfEducation;
+  formOfEducations: FormOfEducation[];
 
   public form: FormGroup = new FormGroup({
     id: new FormControl({value: '', disabled: true}),
@@ -19,19 +26,26 @@ export class GroupAddComponent implements OnInit {
     second: new FormControl(''),
     third: new FormControl(''),
     fourth: new FormControl(''),
-    fifth: new FormControl(''),
+    fifth: new FormControl('0'),
     sixth: new FormControl(''),
     seventh: new FormControl(''),
     eight: new FormControl(''),
     ninth: new FormControl(''),
-    tenth: new FormControl(''),
-    last: new FormControl('', Validators.required)
+    tenth: new FormControl('')
   });
 
-  constructor(private valueService: GroupService) { }
+  constructor(private valueService: GroupService, private formOfEducationService: FormOfEducationService) { }
 
   // tslint:disable-next-line:typedef
   ngOnInit() {
+    this.loadFormOfEducation();
+  }
 
+  // tslint:disable-next-line:typedef
+  loadFormOfEducation() {
+    this.formOfEducationService.getValues()
+      .subscribe((data: FormOfEducation[]) => {
+        this.formOfEducations = data;
+      });
   }
 }
