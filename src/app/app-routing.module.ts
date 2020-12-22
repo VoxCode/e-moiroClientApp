e8ac5a636039083aaafe61f7e79fe36ef6aac5e0
Kezik.92 <Kezik.92@LAPTOP-4>
2020-12-22 17:09:53 +0300

    v 0.9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,6 +23,7 @@ import {TestWorkComponent} from './test-work/test-work.component';
 import {CertificationTypeComponent} from './certification-type/certification-type.component';
 import {FinalExaminationComponent} from './final-examination/final-examination.component';
 import {TrainingProgramAddFormComponent} from './training-program-add-form/training-program-add-form.component';
+import {TrainingProgramAddForm2Component} from './training-program-add-form2/training-program-add-form2.component';
 
 const routes: Routes = [
   { path: 'docxGenerator/:id', component:  DocxGeneratorComponent, canActivate: [AuthGuardService]},
@@ -45,6 +46,7 @@ const routes: Routes = [
   { path: 'trainingProgramAddForm/:id', component: TrainingProgramAddFormComponent, canActivate: [AuthGuardService]},
   { path: 'testWork', component: TestWorkComponent, canActivate: [AuthGuardService]},
   { path: 'curriculumTopicAddForm/:id', component: CurriculumTopicAddFormComponent, canActivate: [AuthGuardService]},
+  { path: 'curriculumTopicAddForm2/:id', component: TrainingProgramAddForm2Component, canActivate: [AuthGuardService]},
   { path: 'finalExamination', component: FinalExaminationComponent, canActivate: [AuthGuardService]},
   { path: 'register', component: RegisterComponent}
 ];
--- src/app/training-program-add-form2/training-program-add-form2.component.ts
+++ src/app/training-program-add-form2/training-program-add-form2.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {ActivatedRoute} from '@angular/router';
+import {TrainingProgram} from "../models/TrainingProgram";
+import {TrainingProgramService} from "../services/training-program.service";
 
 @Component({
   selector: 'app-training-program-add-form2',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./training-program-add-form2.component.scss']
 })
 export class TrainingProgramAddForm2Component implements OnInit {
+  todo = [];
+  done = [];
+  id: number;
+  trainingProgram: TrainingProgram;
 
-  constructor() { }
+  constructor(
+    private trainingProgramService: TrainingProgramService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params.id;
+  }
+
+  // tslint:disable-next-line:typedef
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+
+  // LOAD
+
+  // tslint:disable-next-line:typedef
+  loadTrainingProgram() {
+    this.trainingProgramService.getValue(this.id)
+      .subscribe((data: TrainingProgram) => {
+        if (data !== undefined){
+          this.trainingProgram = data;
+        }
+      });
   }
 
 }
