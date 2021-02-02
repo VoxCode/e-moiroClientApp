8b25e0c26312f6fac4a1687073733661ab4a2336
Андрей Кезик <kezik.1992@gmail.com>
2021-02-02 17:01:13 +0300

    v 0.9

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44358/'
+  // apiUrl: 'https://localhost:44358/'
   // apiUrl: 'http://api.e.moiro.by/'
+  apiUrl: 'http://localhost:5000/'
 };
 
 /*
