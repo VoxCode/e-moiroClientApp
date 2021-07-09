d0a2ad102f0f8092d9d870cb8e09c7eda8b8b3ff
Андрей Кезик <kezik.1992@gmail.com>
2021-07-09 16:00:56 +0300

    v 0.9.3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44358/' // ---IIS
-  // apiUrl: 'http://api.smart.moiro.by/'
+  // apiUrl: 'https://localhost:44358/' // ---IIS
+  apiUrl: 'http://api.smart.moiro.by/'
   // apiUrl: 'http://localhost:5009/'  // ---Kestrel
 };
 
