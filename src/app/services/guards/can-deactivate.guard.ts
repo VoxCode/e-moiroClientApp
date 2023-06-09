f6149eeef20340f2afbcfc1006ff9b12db9afb0f
Onokin <Aleksey.onohov@mail.ru>
2023-06-09 15:05:09 +0300

    v 0.9.5 can deactivate guard added

---
+++ src/app/services/guards/can-deactivate.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+export interface ICanComponentDeactivate {
+  canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CanDeactivateGuard implements CanDeactivate<ICanComponentDeactivate> {
+  canDeactivate(
+    component: ICanComponentDeactivate,
+    currentRoute: ActivatedRouteSnapshot,
+    currentState: RouterStateSnapshot,
+    nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return component.canDeactivate ? component.canDeactivate() : true;
+  }
+
+}
